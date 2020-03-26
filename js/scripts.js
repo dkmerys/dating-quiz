de0a1b54728b8394435e5a6ff879d50f07f68022
Daniel Merys <dkmerys@gmail.com>
2020-03-26 13:03:35 -0700

    create html, cs, js files and set up branches based on user inputs

---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+
+
+$(document).ready(function() {
+
+
+  $("form#dating").submit(function(event) {
+    
+    var gender = $("select#gender").val();
+    var height = $("select#height").val();
+    var intelligence = $("select#intelligence").val();
+    var yourCelebrity = " ";
+
+    
+    // if (gender === "female") { 
+    //   if (height === 'tall') {
+    //     if (intelligence === 'smart') {
+    //       return CelebrityA
+    //     }
+    //   } else if () {
+
+    //   }
+    // }
+    // } else if (height === "tall") { return null
+    // } else (intelligence === "smart") { return null
+    //   yourCelebrity = "CelebrityA";
+    //   alert(yourCelebrity);
+    // }
+    
+    if (gender === "female" && height === "short" && intelligence === "smart"){
+      yourCelebrity = "CelebrityA";
+    };
+    
+    if (gender === "female" && height === "tall" && intelligence === "dumb"){
+      yourCelebrity = "CelebrityB";
+    };
+    
+    if (gender === "female" && height === "short" && intelligence === "dumb"){
+      yourCelebrity = "CelebrityC";
+    };
+    
+    if (gender === "male" && height === "tall" && intelligence === "smart"){
+      yourCelebrity = "CelebrityD";
+    };
+    
+    if (gender === "male" && height === "short" && intelligence === "smart"){
+      yourCelebrity = "CelebrityE";
+    };
+    
+    if (gender === "male" && height === "tall" && intelligence === "dumb"){
+      yourCelebrity = "CelebrityF";
+    };
+    
+    if (gender === "male" && height === "short" && intelligence === "dumb"){
+      yourCelebrity = "CelebrityG";
+    };
+    console.log(yourCelebrity);
+    
+    // $("#partner").empty().append(yourCelebrity);
+    // $("#match").show();
+
+    event.preventDefault();
+  });
+});
